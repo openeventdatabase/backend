73d5dd5eabca370983f97792a5af5716a3d02053
cquest <cquest@openstreetmap.fr>
2016-06-19 16:30:13 +0200

    accept + in URL for timezones

--- backend.py
+++ backend.py
@@ -110,9 +110,10 @@ class EventResource(BaseEvent):
 
 
     def relative_time(self, when, cur):
+        when = when.upper().replace(' ','+')
         event_start = cur.mogrify("%s",(when,)).decode("utf-8")
         event_stop  = cur.mogrify("%s",(when,)).decode("utf-8")
-        when = when.upper()
+
         if when == 'NOW':
             event_start = "now()"
             event_stop  = "now()"
